75db4798ee6f9934a7cec6ed932b4e2f75c4b58f
Renan Cammarosano <renancamm@gmail.com>
2017-09-15 19:09:24 +0100

    Updated staticfiles to handle dev testing

--- mozblog/urls.py
+++ mozblog/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from django.contrib import admin
 
@@ -23,4 +25,4 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
